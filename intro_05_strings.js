0b6d4ee88d32bd84c9d84b4b8588d6ccef89391d
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 15:52:07 +0500

    extend string methods

--- intro_05_strings.js
+++ intro_05_strings.js
@@ -1,14 +1,16 @@
 
-let name = "zeeshan"
+// string is immutable, channot change a single character
+
+const name = "zeeshan"
 console.log(name)
 console.log(name.length)
 
-let name2 = 'azhar' //also can used single qoutes
+const name2 = 'azhar' //also can used single qoutes
 console.log(name, name2)
 
 //template literals 
-let boy1 = "zeeshan"
-let boy2 = "khubaib"
+const boy1 = "zeeshan"
+const boy2 = "khubaib"
 console.log(`${boy1} is a friend of ${boy2}`)
 
 //escape squenece
@@ -22,5 +24,8 @@ console.log(name.slice(2,5)) // start is included but end is not
 console.log(name.slice(5)) // taking start and going to end
 console.log(name.replace('zee','tee'))
 console.log("    zeeehan  ".trim()) //removes wide-spaces
-
+console.log(name.startsWith("zee")); // Check if the string starts with a specific substring
+console.log(name.endsWith("shan")); // Check if the string ends with a specific substring
+console.log(name.includes("ee")); // Check if the string includes a specific substring
+console.log(name.charAt(2)); // Get character at a specific index
 
