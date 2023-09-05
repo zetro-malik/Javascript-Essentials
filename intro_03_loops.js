dcf98ec5e3eb4379987c22200439ade12761dc62
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 15:06:54 +0500

    for in and of loop

--- intro_03_loops.js
+++ intro_03_loops.js
@@ -3,3 +3,22 @@
 for (let i = 0; i < 5; i++) {
     console.log(i)    
 }
+
+//for in loop
+
+let obj = {
+    name:"zeeshan",
+    age:12,
+    gender:"male"
+}
+
+for (let a in obj){
+    console.log(a, obj[a])
+}
+
+//for of loop
+
+//need to be iterable to work
+for (let a of obj){
+    console.log(a)
+}
