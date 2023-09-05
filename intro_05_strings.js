7d222a5aa1cab02d640ebb20f03e0615afa98097
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 15:29:26 +0500

    template literals, esscape sequence

--- intro_05_strings.js
+++ intro_05_strings.js
@@ -5,3 +5,15 @@ console.log(name.length)
 
 let name2 = 'azhar' //also can used single qoutes
 console.log(name, name2)
+
+//template literals 
+let boy1 = "zeeshan"
+let boy2 = "khubaib"
+console.log(`${boy1} is a friend of ${boy2}`)
+
+//escape squenece
+console.log("zeeshan\'s laptop")
+
+
+//string methods
+
