94c6487fa672dfc76b65f90e16604369f19fc765
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 15:01:26 +0500

    intro_02_expersions

---
+++ intro_02_expersions.js
@@ -0,0 +1,41 @@
+//#region operators in JS
+
+// arthemitics operators
+let a = 45
+let b = 4
+console.log("a + b =",a+b)
+console.log("a - b =",a-b)
+console.log("a * b =",a*b)
+console.log("a / b =",a/b)
+console.log("a ** b =",a**b)
+console.log("a % b =",a%b)
+console.log("a++ =",a++) //depends is ++ is after or before the variable
+console.log("a-- =",a--)
+
+//assignment operators
+let ass = 5
+ass += 5
+console.log(ass)
+
+//comparios operator
+let comp1 = 5;
+let comp2 = 5;
+console.log('a == b', comp1 == comp2)
+console.log('a != b', comp1 != comp2)
+console.log('a === b', comp1 === comp2) //also compares datatype
+console.log('a !== b', comp1 !== comp2) //also compares datatype
+
+
+// logical operators
+console.log('true && false', true && false)
+console.log('true || false', false || true)
+console.log('!true', !true)
+
+
+//ternary operator
+let age = 18
+let canDrive = age >= 18 ? "can drive" : "cannot drive" 
+console.log(canDrive)
+
+//#endregion
+
