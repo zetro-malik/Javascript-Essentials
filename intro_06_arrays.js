63171b2e509c35635a8a84f4f8d88c35eb9a608e
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 16:04:50 +0500

    changes in function

---
+++ intro_06_arrays.js
@@ -0,0 +1,6 @@
+const fruits = ["banana","apple","peach"]
+let dynmc = [1,'hello',true,{'obj':true}] // can store multiple types
+console.log(dynmc)
+console.log(dynmc.length)
+dynmc[1]  = "zeeshan" // are mutable
+console.log(dynmc)
