c377c63fe49b1d098b44159caf1fad5abb8a8fd4
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 12:48:54 +0500

    variables intro

---
+++ intro_01_variables.js
@@ -0,0 +1,20 @@
+/* dynamically typed language
+ can change datatype of variable on runtime
+ $, letter, uderscore are allowed for decleration*/
+
+
+ let a = 67
+ console.log(a)
+ a = "harry"
+ console.log(a)
+
+
+ const b = 67  // only be initialized in the as creation
+ console.log(a)
+ //b = "harry" //throws error
+ console.log(a)
+
+
+ // difference between var, let, const
+
+ 
