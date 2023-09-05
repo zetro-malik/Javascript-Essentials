9a5a2491aca5f1b750d20524c82a94cf3374e2cd
zetro-malik <zeeshanimsb@gmail.com>
2023-09-05 14:29:26 +0500

    primitive and non primtive datatype

--- intro_01_variables.js
+++ intro_01_variables.js
@@ -1,3 +1,4 @@
+//#region dynamically typed
 /* dynamically typed language
  can change datatype of variable on runtime
  $, letter, uderscore are allowed for decleration*/
@@ -14,6 +15,10 @@
  //b = "harry" //throws error
  console.log(a)
 
+//#endregion
+
+
+//#region var, let and const
 
 //  difference between var, let, const, before ES6 var was used, but not used now because it makes global variable
 //  let used to make block-spaced variable
@@ -31,3 +36,37 @@ var dd = undefined
 
 // console.log(inside) throws error, not inside the block
 
+//#endregion
+
+
+//#region primitive datatype and objects
+
+// 7- primitive datatypes null, number, string, symbol, undefined, boolean, byte
+// let is undefined, if not initialzed
+
+
+// primitive datatype
+let aaa = null
+let bbb = 423
+let ccc = true
+let ddd = BigInt("100") + BigInt("5")
+let eee = "Harry"
+let fff = Symbol('I am a nice symbol') 
+let ggg = undefined
+
+console.log(fff,bbb)
+console.log(typeof eee)  //finding type of datatype
+
+
+// non-primitive datatype Objects in JS are like dictionary in python, and map in flutter
+const item = {
+    "Harry":true,
+    "Shub":false,
+    "lovish":65,
+    "rohan":undefined
+}
+
+console.log(item['lovish'])
+
+
+//#endregion
