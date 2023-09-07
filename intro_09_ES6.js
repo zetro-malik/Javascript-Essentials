5f262217aa0df738c9e1fb39bcd9778f9e06e800
zetro-malik <zeeshanimsb@gmail.com>
2023-09-07 12:44:01 +0500

    desturcturing an element

--- intro_09_ES6.js
+++ intro_09_ES6.js
@@ -1,4 +1,4 @@
-
+console.log("-------functions-------")
 //arrow functions
 var func = ()=>{};
 
@@ -16,4 +16,45 @@ func4(10)
 func4()
 
 
-//
+// spread and rest operators
+console.log("-------spread operator-------")
+//speard, spread all values of element, used for copy or to spread values
+var a =[1,2,3,4,5]
+var b = [...a]
+console.log(b)
+
+//rest is used to allows you to collect multiple elements into a single array.
+console.log("-------rest operator-------")
+var restFunc = (a,b,c)=>{
+    console.log(a,b,c)
+}
+
+restFunc(1,2,3,4,5,6,7,8,9) // a=1, b=2, c= will hold all remaining values
+
+
+// destructuring and element
+console.log("-------Destructuring-------")
+a = [1,2,3]
+var [val1,val2] = a
+console.log(val1,val2)
+
+//desturing an object 
+
+b = {
+    prompt:function(){console.log("using prompt")},
+    alert:function(){console.log("using alert")}
+}
+
+var { alert} = b
+console.log(alert())
+
+
+//try catch
+
+console.log('hey')
+try{
+    console.log(hey)
+}catch(e){
+    console.log(e)
+}
+console.log("hey")
