6531bb1c4b6f748e2ca86adc5b9775abe9192470
zetro-malik <zeeshanimsb@gmail.com>
2023-09-14 15:10:55 +0500

    express started

---
+++ intro_13_express/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const req = require('express/lib/request')
+
+const app = express()
+const PORT = 5550
+
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    // automatically sets the headers
+   res.send('server has started')
+})
