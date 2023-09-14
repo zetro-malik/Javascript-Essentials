c3092585dc2c2f16bdaa443d99b74f20a189765c
zetro-malik <zeeshanimsb@gmail.com>
2023-09-14 15:17:19 +0500

    add sendhtml, sendjson

--- intro_13_express/index.js
+++ intro_13_express/index.js
@@ -15,6 +15,20 @@ app.get('/',(req,res)=>{
    res.send('server has started')
 })
 
+
+app.get('/sendHtml',(req,res)=>{
+    res.setHeader('Content-Type','text/html')
+    res.statusCode = 200;
+   console.log(path.join(__dirname,"index.html"))
+   res.sendFile(path.join(__dirname,"index.html"))
+})
+
+app.get('/sendJson',(req,res)=>{
+    const page = fs.readFileSync('D:\\GitHub repos\\nodeJS_Todo\\nodejs\\a.json')
+    const jsonData = JSON.parse(page);
+    res.json(jsonData);
+})
+
 app.listen(PORT,()=>{
     console.log(`server started at PORT = ${PORT}`)
 })
