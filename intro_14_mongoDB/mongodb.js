0b111d2d2f2914aeab8eb58eb320f0058e2d2e49
zetro-malik <zeeshanimsb@gmail.com>
2023-09-16 13:52:50 +0500

    basics of aggregate

--- intro_14_mongoDB/mongodb.js
+++ intro_14_mongoDB/mongodb.js
@@ -30,11 +30,13 @@ async function addDocument() {
   const db = client.db("nodejs")
   const customers = db.collection("customers");
   const products = db.collection("products");
+  const teachers = db.collection('teachers');
 
 
   //delete if there any record previously added
   await customers.deleteMany({});
   await products.deleteMany({});
+  await teachers.deleteMany({});
 
 
   //takes in a document : insertone takes document
@@ -60,6 +62,33 @@ async function addDocument() {
     { name: 'Viola', address: 'Sideway 1633' }
   ]);
   console.log("customers added successfully", res)
+
+
+  res = await teachers.insertMany([
+    { "_id": 1, "name": "John", "age": 33, "gender": "male" },
+    { "_id": 2, "name": "Alice", "age": 32, "gender": "female" },
+    { "_id": 3, "name": "Bob", "age": 41, "gender": "male" },
+    { "_id": 4, "name": "Emma", "age": 56, "gender": "female" },
+    { "_id": 5, "name": "David", "age": 33, "gender": "male" },
+    { "_id": 6, "name": "Olivia", "age": 22, "gender": "female" },
+    { "_id": 7, "name": "Sophia", "age": 56, "gender": "female" },
+    { "_id": 8, "name": "Liam", "age": 56, "gender": "male" },
+    { "_id": 9, "name": "Noah", "age": 30, "gender": "male" },
+    { "_id": 10, "name": "Ella", "age": 41, "gender": "female" },
+    { "_id": 11, "name": "Daniel", "age": 56, "gender": "male" },
+    { "_id": 12, "name": "Grace", "age": 27, "gender": "female" },
+    { "_id": 13, "name": "Ethan", "age": 29, "gender": "male" },
+    { "_id": 14, "name": "Mia", "age": 31, "gender": "female" },
+    { "_id": 15, "name": "William", "age": 45, "gender": "male" },
+    { "_id": 16, "name": "Ava", "age": 56, "gender": "female" },
+    { "_id": 17, "name": "Benjamin", "age": 33, "gender": "male" },
+    { "_id": 18, "name": "Sophie", "age": 29, "gender": "female" },
+    { "_id": 19, "name": "James", "age": 33, "gender": "male" },
+    { "_id": 20, "name": "Charlotte", "age": 24, "gender": "female" }
+]
+);
+console.log("teachers added successfully", res)
+
 
   //in res, getting back the documentID 
 
@@ -136,6 +165,9 @@ async function filterDocument() {
   }).toArray();
   console.log("Name equals 'John' OR Age greater than or equal to 30:", result);
 
+
+  result = await customers.find({}).limit(2).toArray();
+  console.log("limiting the result = ", result);
 }
 
 
@@ -153,51 +185,224 @@ async function sortDocument() {
   console.log('display names in decending order', result)
 }
 
-async function deleteDocument(){
+async function deleteDocument() {
   const db = client.db("nodejs");
   const customers = db.collection('customers');
   const products = db.collection('products');
 
   //Delete the document with the address "Mountain 21":
-  let result = await customers.deleteOne({address:"Mountain 21"});
-  console.log("deleted one document ",result);
+  let result = await customers.deleteOne({ address: "Mountain 21" });
+  console.log("deleted one document ", result);
 
   //Delete all documents were the address starts with the letter "O":
-  result = await customers.deleteMany({address:/^O/});
-  console.log("deleted all document startig with O",result);
+  result = await customers.deleteMany({ address: /^O/ });
+  console.log("deleted all document startig with O", result);
 
 }
 
-async function dropDocument(){
+async function dropDocument() {
   const db = client.db("nodejs");
   await db.dropCollection('customers')
   console.log('customers dropped successfuly');
 }
 
-async function updateDocument(){
+async function updateDocument() {
   const db = client.db("nodejs");
   const customers = db.collection('customers');
   const products = db.collection('products');
 
   //first parameter is for filtering, second to set new values
-   let result =await customers.updateOne({ address: "Valley 345" },{ $set: {name: "Mickey", address: "Canyon 123" } })
-   console.log("updating one document =",result)
+  let result = await customers.updateOne({ address: "Valley 345" }, { $set: { name: "Mickey", address: "Canyon 123" } })
+  console.log("updating one document =", result)
 
-   result = await customers.updateMany({address : /^S/},{$set:{name:"Minnie"}})
-   console.log('updating many documents',result)
+  result = await customers.updateMany({ address: /^S/ }, { $set: { name: "Minnie" } })
+  console.log('updating many documents', result)
 
-   result = await products.updateOne({ _id: 154 },{ $set: { price: 19.99 }});
-   console.log("Updating product price by ID and decrementing quantity:", result);
- 
-   // Update prices for products within a certain price range and increment stock
-   result = await products.updateMany({ price: { $gte: 50, $lte: 100 } },{ $inc: { price: 10 }});
-   console.log("Increasing prices and stock for products in the 50-100 range:", result);
+  result = await products.updateOne({ _id: 154 }, { $set: { price: 19.99 } });
+  console.log("Updating product price by ID and decrementing quantity:", result);
+
+  // Update prices for products within a certain price range and increment stock
+  result = await products.updateMany({ price: { $gte: 50, $lte: 100 } }, { $inc: { price: 10 } });
+  console.log("Increasing prices and stock for products in the 50-100 range:", result);
+
+  //rename column name
+  result = await products.updateMany({}, { $rename: { name: "pname" } });
+  console.log("renaming attribute name = ", result);
+}
+
+
+async function aggregateDocument() {
+  const db = client.db("nodejs");
+  const teachers = db.collection('teachers');
+  //aggregate first argument is pipeline(list of operations) and 2nd is options
+  //$match, $group, $sort are operators of a pipeline
+  let result = await teachers.aggregate([{$match:{gender:"male"}}]).toArray(); 
+  console.log("using aggrate to filter male teachers",result);
+
+  //group teacher by age, also shows name
+  //$group takes , _id:(on which grouping be performed), fields to include in that group=> syntax names:{$push:"$name"} pushing all match "$name" in names  
+  result = await teachers.aggregate([
+    {
+      $group: {
+        _id: "$age",
+        data: {
+          $push: {
+            name: "$name",
+            gender: "$gender" // Include gender along with the name
+          }
+          // $push: "$$ROOT" brings in full document
+        }
+      }
+    }
+  ]).toArray();
   
-   //rename column name
-    result = await products.updateMany({},{$rename:{name:"pname"}});
-    console.log("renaming attribute name = ",result);
-  }
+  console.log("Using aggregate to group by age teachers", result);
 
+
+  // find count of males teacher group by age
+  // first operation in pipeline [match,group=>count]
+
+  result = await teachers.aggregate([
+    {
+      $match:{gender:"male"}
+    },
+    {
+      $group: {
+        _id: "$age",
+        number: {
+          $sum: 1 
+        }
+      }
+    }
+  ]).toArray();
+  
+  console.log("count of males teacher group by age", result);
+
+  //give a count per age of male student and sort them by count in desc manner
+  // $toDouble operator makes a field double and perfrom inc,dec and sum
+  result = await teachers.aggregate([
+    {
+      $match:{gender:"male"}
+    },
+    {
+      $group: {
+        _id: "$age",
+        number: {
+          $sum: 1 
+        }
+      }
+    },
+    {
+      $sort:{number:-1}
+    },
+  ]).limit(3).toArray();
+  
+  console.log("count of males teacher group by age count in desc manner", result);
+ 
+
+  //unwind works on arrays, makes seperated document of each array object
+
+
+}
+
+async function addStudentData(){
+  const db = client.db("nodejs");
+  const universities = db.collection('universities');
+  const departments = db.collection('departments');
+  const courses = db.collection('courses');
+  const Students  = db.collection('students');
+
+  await universities.insertMany([
+    {
+      "_id": 1,
+      "name": "University A"
+    },
+    {
+      "_id": 2,
+      "name": "University B"
+    }
+  ]);
+
+  await departments.insertMany([
+    {
+      "_id": 101,
+      "name": "Computer Science",
+      "universityId": 1
+    },
+    {
+      "_id": 102,
+      "name": "Mathematics",
+      "universityId": 1
+    },
+    {
+      "_id": 201,
+      "name": "Physics",
+      "universityId": 2
+    },
+    {
+      "_id": 202,
+      "name": "Chemistry",
+      "universityId": 2
+    }
+  ]
+  );
+
+  await courses.insertMany([
+    {
+      "_id": 1001,
+      "courseName": "Intro to Programming",
+      "departmentId": 101
+    },
+    {
+      "_id": 1002,
+      "courseName": "Database Management",
+      "departmentId": 101
+    },
+    {
+      "_id": 1003,
+      "courseName": "Calculus",
+      "departmentId": 102
+    },
+    {
+      "_id": 1004,
+      "courseName": "Quantum Mechanics",
+      "departmentId": 201
+    },
+    {
+      "_id": 1005,
+      "courseName": "Organic Chemistry",
+      "departmentId": 202
+    }
+  ]
+  );
+
+  await Students.insertMany([
+    {
+      "_id": 10001,
+      "name": "John",
+      "age":22,
+      "courses": [1001, 1003],
+      "score":[55,22]
+    },
+    {
+      "_id": 10002,
+      "name": "Alice",
+      "age":23,
+      "courses": [1001, 1002],
+      "score":[47,72]
+    },
+    {
+      "_id": 10003,
+      "name": "Bob",
+      "age":22,
+      "courses": [1003],
+      "score":[35]
+    }
+  ]
+  );
+
+  console.log('documents added succesfully');
+}
 
 
 
@@ -215,4 +420,8 @@ async function updateDocument(){
 
 //baseClientConnect(dropDocument)
 
-baseClientConnect(updateDocument)
+//baseClientConnect(updateDocument)
+
+//baseClientConnect(aggregateDocument)
+
+//baseClientConnect(addStudentData)   // new data for $lookup
